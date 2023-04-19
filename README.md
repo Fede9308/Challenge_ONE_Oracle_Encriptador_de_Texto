2b0afae942360cae32474b382eead77da9ab019a
Federico <fedefbecerra@gmail.com>
2023-04-19 00:09:31 -0300

    Version con funcionalidades terminada

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Challenge_ONE_Oracle_Encriptador_de_Texto
-This is the first challenge of the course of ONE wich consist in a web were the user can encrypt a text  
+This is the first challenge of the course of ONE(Oracle Next Education), with Alura Latam, wich consist in a web were the user can encrypt a text with the "keys" given, then the text generated can be copied with a button to the clipboard.
+
--- assets/index.js
+++ assets/index.js
@@ -3,9 +3,6 @@ const input= document.querySelector('textarea')
 const encriptar = document.getElementById('encriptar') 
 const desencriptar = document.getElementById('desencriptar') 
 const busquedaTexto = document.forms[0] 
-/* const textoEncriptado= document.querySelector('#encriptado') */
-/* const btnPcopiar= document.getElementById('btnCopiar') */
-
 
 
 dibujarResultado(input.value)
@@ -17,55 +14,14 @@ busquedaTexto.addEventListener('submit', (event)=>{
 
 encriptar.addEventListener('click', ()=>{
     encriptacion(input.value) 
-    console.log("contador1")
 })
 desencriptar.addEventListener('click',()=>{
     desencriptacion(input.value)
-    console.log("contador2") 
 })
 
-/* const observer= new MutationObserver(() =>{
-    const btnPcopiar= document.getElementById('btnCopiar');
-    if (btnPcopiar){
-        btnPcopiar.addEventListener('click', () => {
-            console.log("contador3")
-        })
-        observer.disconnect();
-    }
-}); */
-
-/* btnPcopiar.addEventListener('click', ()=>{
-    console.log("contador3")
-}) */
-
-/* btnPcopiar.addEventListener('click',()=> {
-    navigator.clipboard.writeText(textoEncriptado.innerText).then(() => {
-        console.log('Texto copiado al portapapeles');
-      }).catch((error) => {
-        console.error('Error al copiar el texto al portapapeles:', error);
-      });
-}) */;
-
-/* async function espera(){
-    await dibujarResultado();
-
-    
-
-    btnPcopiar.onclick = console.log("contador3")
-} */
-
-
-
-
-
 function dibujarResultado(entrada) {
-    let resultados =''
-    if (entrada.length == 0) {
-    /*     const textoFallido = document.createElement('div')
-        textoFallido.innerHTML = '<img src="./assets/images/Muñeco.png" alt="Muñeco"><article><h3>Ningún mensaje fue encontrado</h3><p>Ingresa el texto que desees encriptar o desencriptar.</p> </article>' ;
-       
-       resultado.appendChild(textoFallido) */
-       
+    let resultados ='';
+    if (entrada.length == 0) {       
          resultados += 
         `<img src="./assets/images/Muñeco.png" alt="Muñeco">
         <article>
@@ -73,8 +29,8 @@ function dibujarResultado(entrada) {
             <p>Ingresa el texto que desees encriptar o desencriptar.</p>
         </article>`
         resultado.innerHTML=resultados;
-        
-        return
+        input.focus();
+        return;
     } 
     else{
         resultados = '';
@@ -88,28 +44,19 @@ function dibujarResultado(entrada) {
         resultado.appendChild(textoEncriptado);
         resultado.appendChild(btnPcopiar);
 
-     
+        
         
             btnPcopiar.addEventListener('click',()=> {
                 navigator.clipboard.writeText(textoEncriptado.innerText).then(() => {
                     console.log('Texto copiado al portapapeles');
+                    input.focus();
                   }).catch((error) => {
                     console.error('Error al copiar el texto al portapapeles:', error);
                   });
-            }) 
-
-             /*   btnPcopiar.addEventListener('click', ()=>{
-            console.log("contador3")}); */
+            }); 
         
-       /*  resultados += `<h3 id="encriptado">${entrada}</h3>
-        <button class="botonClaro" id="btnCopiar">Copiar</button>`; */
-        
-       
     }
     
-    
-
-   /* textoFallido.parentNode.removeChild(textoFallido) */
 }
 
 function encriptacion(texto) {
@@ -121,7 +68,6 @@ function encriptacion(texto) {
             let texto1=texto.toLowerCase();
 
         let array1= texto1.split('');
-    /*  console.log(array1) */
         let array2=[];
 
         array1.forEach(element => {
@@ -148,7 +94,6 @@ function encriptacion(texto) {
             }
         });
         let textoEncriptado = array2.join('')
-        /* console.log(textoEncriptado) */
         dibujarResultado(textoEncriptado)
     }
 }
@@ -162,60 +107,22 @@ function desencriptacion(texto) {
     }
     else{
         let texto1=texto.toLowerCase();
-        /* console.log(texto1) */
+        
         let text = texto1
         .replace(/enter/g, "e")
         .replace(/imes/g, "i")
         .replace(/ai/g, "a")
         .replace(/ober/g, "o")
         .replace(/ufat/g, "u");
-        /* console.log(text); */
+       
         dibujarResultado(text);
     }
  
 }
 
+// Se agrego una funcione que prevenga el uso de acentos
+
 function tieneAcento(texto) {
     const regulador = /[áéíóú´]/i;
     return regulador.test(texto);
-  }
-
-
-
-/*   const observer= new MutationObserver(() =>{
-    const btnPcopiar= document.getElementById('btnCopiar');
-    if (btnPcopiar){
-        btnPcopiar.addEventListener('click', () => {
-            console.log("contador3")
-        })
-        observer.disconnect();
-    }
-});
-observer.observe(document.body, { childList: true });
- */
-/* function copyText (htmlElement){
-    if(!htmlElement){
-        return;
-    }
-
-    let elementText= htmlElement.innerText;
-
-    let inputElement = document.createElement('input');
-    inputElement.setAttribute('value', elementText);
-    document.body.appendChild(inputElement);
-    const contenido=inputElement;
-    const cb=navigator.clipboard;
-    cb.writeText(contenido.value)
-    console.log(cb)
-    inputElement.select();
-    document.execCommand('copy');
-   inputElement.parentNode.removeChild(inputElement);
-
-} */
-/* Las "llaves" de encriptación que utilizaremos son las siguientes:
-
-La letra "e" es convertida para "enter"
-La letra "i" es convertida para "imes"
-La letra "a" es convertida para "ai"
-La letra "o" es convertida para "ober"
-La letra "u" es convertida para "ufat" */
+}
